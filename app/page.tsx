eeb30f77b787ec41f7c589f4a9767e8e06c4b902
juliangay <julian.gay@credprotocol.com>
2024-02-04 14:25:51 -0800

    Small edit

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { NEXT_PUBLIC_URL } from './config';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: 'Tell me the story now', 
+      label: 'Tell me a story', 
     },
     {
       label: 'Redirect to cute dog pictures',
